776af67e868d3a58b783bb5debd92a524d25f76a
Louis Soccard <louis@soccard.uk>
2022-02-02 14:26:24 +0000

    Add MySQL SSL config for database on Azure

--- config/database.php
+++ config/database.php
@@ -58,8 +58,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_KEY    => env('MYSQL_ATTR_SSL_KEY'),
             ]) : [],
         ],
 
